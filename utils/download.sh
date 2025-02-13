2f9191dbd1f51b9825fcc342af9dc893558a0683
Zhang Yuanfeng <z2925653688@qq.com>
2025-02-13 09:16:51 +0800

    use relative path for dbsnp vcf file at resources/dbsnp dir

--- utils/download.sh
+++ utils/download.sh
@@ -1,15 +1,19 @@
 dbsnp_url="https://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh38p7/VCF/00-common_all.vcf.gz"
 
+mkdir -p resources/dbsnp
+cd resources/dbsnp
+
 if command -v aria2c >/dev/null 2>&1; then
   aria2c \
       -c -x 4 -k 1M \
       "${dbsnp_url}" \
-      -o "resources/common_all.vcf.gz"
+      -o "common_all.vcf.gz"
 else
-  wget -c "${dbsnp_url}" -o "resources/common_all.vcf.gz"
+  wget -c "${dbsnp_url}" -o "common_all.vcf.gz"
 fi
 
-cd resources
 zcat common_all.vcf.gz |\
     awk 'BEGIN{OFS="\t"} $1 ~ /^#/ {print $0; next} {print "chr"$1,$2,$3,$4,$5,$6,$7,$8}' |\
     bgzip -@ 2 -i -I common.dbsnp.vcf.gz.tbi >common.dbsnp.vcf.gz
+
+rm common_all.vcf.gz
